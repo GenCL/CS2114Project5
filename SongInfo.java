2b87655bbbca07765cde73bcbc5aaa39fb7eff87
GenCL <gengze.l92@gmail.com>
2018-11-29 05:07:52 -0500

    Add files via upload

---
+++ SongInfo.java
@@ -0,0 +1,12 @@
+package project5;
+
+/**
+ * Song info represents song's the name, artist, year, genre and people's
+ * answers on this song.
+ * 
+ * @author gengzelyu
+ * @version 2018.11.28
+ */
+public class SongInfo {
+
+}
